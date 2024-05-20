b0151b987e93f9037601ea1823f62491d9556327
Yordii <yokdy3602@gmail.com>
2024-05-20 11:54:49 -0500

    add https to servers

--- server-user/src/app.js
+++ server-user/src/app.js
@@ -1,12 +1,23 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const fs = require("fs");
+const https = require("https");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+const servidorHTTPS = https.createServer(
+  {
+    cert: fs.readFileSync("./../crt/servicioslogisticosrh.crt"),
+    key: fs.readFileSync("./../crt/llave.key"),
+    passphrase: "12345",
+  },
+  app
+);
 
-app.use('/server-user', require('./routes/'))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-module.exports = app
+app.use("/server-user", require("./routes/"));
+
+module.exports = servidorHTTPS;
