daa57d75bb209b752e16c4db07d48bf419e34615
Yordii <yokdy3602@gmail.com>
2024-05-20 11:33:24 -0500

    add message test run

--- server-delivery/src/index.js
+++ server-delivery/src/index.js
@@ -5,5 +5,8 @@ const HOST =
   process.env.NODE_ENV == "development" ? "localhost" : process.env.HOSTNAME;
 
 servidorHTTPS.listen(PORT, () => {
-  console.log(`http://${HOST}:${PORT}/server-delivery`);
+  console.log(
+    `http://${HOST}:${PORT}/server-delivery`,
+    "Host db Principal:" + process.env.DB_APAL_HOST
+  );
 });
